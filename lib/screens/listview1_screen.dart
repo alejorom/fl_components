e718a84af1f1224a7849ab86530042564f5db8c0
alejorom <morojela-github@yahoo.com>
2022-02-04 14:17:50 -0500

    Agregar ListView/ListTile

--- lib/screens/listview1_screen.dart
+++ lib/screens/listview1_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class Listview1Screen extends StatelessWidget {
   const Listview1Screen({Key? key}) : super(key: key);
 
+  final opciones = const ['Dirt', 'Formula 1', 'RR3', 'CSR2'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,9 +12,15 @@ class Listview1Screen extends StatelessWidget {
         title: const Text('ListView #1'),
       ),
       body: ListView(
-        children: const [
-          Text('Hallo Mundo'),
-          Text('Hallo Mundo'),
+        children: [
+          ...opciones
+              .map(
+                (game) => ListTile(
+                  title: Text(game),
+                  trailing: const Icon(Icons.arrow_forward_ios),
+                ),
+              )
+              .toList(),
         ],
       ),
     );
