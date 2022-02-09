407f4a8053333f2cab4fcfc18873655d50d02bf3
alejorom <morojela-github@yahoo.com>
2022-02-09 13:33:05 -0500

    Agregar modelo y listado de rutas

--- lib/router/app_routes.dart
+++ lib/router/app_routes.dart
@@ -1,8 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:fl_components/screens/screens.dart';
+import 'package:fl_components/models/models.dart';
 
 class AppRoutes {
   static const initialRoute = 'home';
+
+  static final menuOptions = <MenuOption>[
+    MenuOption(
+      route: 'home',
+      name: 'Home Screen',
+      screen: const HomeScreen(),
+      icon: Icons.home,
+    ),
+    MenuOption(
+      route: 'listview1',
+      name: 'List View #1',
+      screen: const Listview1Screen(),
+      icon: Icons.list,
+    ),
+    MenuOption(
+      route: 'listview2',
+      name: 'List View #2',
+      screen: const Listview2Screen(),
+      icon: Icons.list_sharp,
+    ),
+    MenuOption(
+      route: 'alert',
+      name: 'Alertas o Alerts',
+      screen: const AlertScreen(),
+      icon: Icons.sim_card_alert_rounded,
+    ),
+    MenuOption(
+      route: 'card',
+      name: 'Tarjetas o Cards',
+      screen: const CardScreen(),
+      icon: Icons.card_membership_rounded,
+    ),
+  ];
 
   static Map<String, Widget Function(BuildContext)> routes = {
     "home": (BuildContext context) => const HomeScreen(),
