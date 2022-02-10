b139520aa187b6ec801a20861fe687a5d3e0c1be
alejorom <morojela-github@yahoo.com>
2022-02-10 18:10:17 -0500

    Adicionar card_screen

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
@@ -5,9 +6,32 @@ class CardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('CardScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Tarjetas Espectaculares'),
+        ),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        children: [
+          Card(
+            child: Column(
+              children: const [
+                ListTile(
+                  leading: Icon(
+                    Icons.sanitizer,
+                    color: AppTheme.primaryColor,
+                  ),
+                  title: Text('Soy un título'),
+                  subtitle: Text(
+                    'Región de manos sucias de pinceles sin pelo de niños boca abajo de cepillos de dientes. Zona donde la rata se ennoblece y hay banderas innúmeras y cantan himnos y alguien te prende, una medalla sobre el pecho.',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
