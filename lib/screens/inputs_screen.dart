cc8299bf2da8042f0f2e9e38b7a29768b567371b
alejorom <94083673+alejorom@users.noreply.github.com>
2023-05-23 11:00:24 -0500

    Usar TextBoxFields

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -18,6 +18,25 @@ class InputsScreen extends StatelessWidget {
                 autofocus: true,
                 textCapitalization: TextCapitalization.words,
                 onChanged: (value) {},
+                validator: (value) {
+                  if (value == null) return 'Este campo es requerido';
+                  return value.length < 3 ? 'Mínimo 3 caracteres' : null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                  hintText: 'Nombre de usuario',
+                  labelText: 'Nombre',
+                  helperText: 'Sólo letras',
+                  // prefixIcon: Icon(Icons.verified_user_outlined),
+                  suffixIcon: Icon(Icons.group_outlined),
+                  icon: Icon(Icons.assignment_ind_outlined),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
